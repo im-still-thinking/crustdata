ba875353e8ae1b0db8d5b39320bd0ae67a476a2d
im-still-thinking <royaritra1990rocks@gmail.com>
2025-01-05 23:04:28 +0530

    updated requirements.txt

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,9 +1,10 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY") or st.secrets["GEMINI_API_KEY"]
 # NOTION_TOKEN = os.getenv("NOTION_TOKEN")
 
 # NOTION_DOCS = [
